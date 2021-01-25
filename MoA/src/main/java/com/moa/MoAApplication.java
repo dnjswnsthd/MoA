37890fee240e8425d6933c7f5abb0cbfd186885c
vcxttlvcx <76426334+vcxttlvcx@users.noreply.github.com>
2021-01-25 16:29:04 +0900

    Spring Boot 기본 틀
    - Spring Boot 기본 구성

---
+++ MoA/src/main/java/com/moa/MoAApplication.java
@@ -0,0 +1,13 @@
+package com.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoAApplication.class, args);
+	}
+
+}
