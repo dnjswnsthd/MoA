b6746c69272efe42e4ff3a757781e8b48424bb07
vcxttlvcx <76426334+vcxttlvcx@users.noreply.github.com>
2021-01-26 16:55:23 +0900

    Login 부분 주석 추가
    Login 부분 주석 추가

--- Backend/src/main/java/com/moa/model/mapper/MemberMapper.java
+++ Backend/src/main/java/com/moa/model/mapper/MemberMapper.java
@@ -4,6 +4,12 @@ import java.sql.SQLException;
 
 import com.moa.model.MemberDto;
 
+/**
+ * 2021-01-26
+ * 로그인 맵퍼 구현 - member.xml과 연동
+ * 
+ * @author Team Together
+ */
 public interface MemberMapper {
 	public MemberDto login(MemberDto memberDto) throws SQLException;
 }
