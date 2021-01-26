1dfe7825c01e257bae0946caf6f8fab3bc0922c4
vcxttlvcx <76426334+vcxttlvcx@users.noreply.github.com>
2021-01-26 16:50:46 +0900

    Login 부분 주석 추가
    Login 부분 주석 추가

--- Backend/src/main/java/com/moa/model/service/MemberServiceImpl.java
+++ Backend/src/main/java/com/moa/model/service/MemberServiceImpl.java
@@ -15,6 +15,7 @@ public class MemberServiceImpl implements MemberService {
 	
 	@Override
 	public MemberDto login(MemberDto memberDto) throws Exception {
+		// 아이디와 비밀번호 둘 중 하나라도 입력으로 주어지지 않았다면 null 반환
 		if(memberDto.getId() == null || memberDto.getPw() == null)
 			return null;
 		return sqlSession.getMapper(MemberMapper.class).login(memberDto);
