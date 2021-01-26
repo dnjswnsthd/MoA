1807b884407521f03eafb0dca07c49a99a348665
minju75 <68409296+minju75@users.noreply.github.com>
2021-01-26 17:11:04 +0900

    header(0126_2차)

--- Frontend/moa/src/components/common/MainHeader.vue
+++ Frontend/moa/src/components/common/MainHeader.vue
@@ -1,44 +1,66 @@
 <template>
-    <header>
+    <header >
         <v-container class="col-8">
             <v-row>
                 <div><img src="@/assets/images/main/logo.png" alt="로고" style="width: 150px; height: 60px;"></div>
                 <v-spacer></v-spacer>                
                     <li class="width-120 centerText height-70">
-                    <router-link to="/foo">펀딩 오픈</router-link>
+                    <router-link to="/FundingOpen">펀딩 오픈</router-link>
                     </li>
                     <li class="width-120 centerText height-70">
-                    <router-link to="/foo">My Page</router-link>
+                    <router-link to="/MyPage">My Page</router-link>
                     </li>
                     <li class="width-120 centerText height-70">
                     <router-link to="/foo">로그아웃</router-link>
                     </li>
-            </v-row>          
+            </v-row> 
+            <div class="col-12">
+                <v-row>
+                    <div class="col-6">
+                        <h1>"최고의 팀" <br> 꾸려보고 싶으신가요?</h1>
+                    </div>
+                    
+                    <div class="col-6">
+                        <div class="searchBox col-6">
+                            <input type="text"><v-icon>mdi-magnify</v-icon>
+                        </div>
+                        
+                    </div>
+               </v-row>
+            </div>
+                     
         </v-container>
-
-        <v-container class="col-8">
-            <v-row>
-                <div><img src="@/assets/images/main/logo.png" alt="로고" style="width: 150px; height: 60px;"></div>
-                <v-spacer></v-spacer>                
-                    <li class="width-120 centerText height-70">
-                    <router-link to="/foo">회원가입</router-link>
-                    </li>
-                    <li class="width-120 centerText height-70">
-                    <router-link to="/foo">로그인</router-link>
-                    </li>
-            </v-row>          
-        </v-container>
-
         
     </header>
 </template>
 
 <script>
+
+
 export default {
-    name: "MainHeader"
+    name: "Header"
 }
 </script>
 
 <style>
-
+    .width-120 {
+        width: 120px;
+    }
+    .height-70 {
+        height: 70px;
+    }
+    .centerText {
+        text-align: center;
+    }
+    header .container .row li {
+        line-height: 70px;
+    }
+    header .container .row li a {
+        color: black;
+    }
+    .searchBox {
+        border: 1px solid black;
+        margin: 0 auto;
+        border-radius: 10px;
+    }
 </style>
