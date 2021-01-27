561fe7b9519195c4e068a47a938c3875e4a90706
vcxttlvcx <76426334+vcxttlvcx@users.noreply.github.com>
2021-01-27 15:24:55 +0900

    아이디 중복 검사 완성
    - 아이디 중복 검사
    - 중복 되지 않을 경우 "message"로 "success" 전달
    - 중복 될 경우 "message"로 "fail" 전달

--- Backend/src/main/java/com/moa/model/service/MemberService.java
+++ Backend/src/main/java/com/moa/model/service/MemberService.java
@@ -1,10 +1,8 @@
 package com.moa.model.service;
 
-import java.util.Map;
-
 import com.moa.model.MemberDto;
 
 public interface MemberService {
 	public MemberDto login(MemberDto memberDto) throws Exception;
-	public boolean checkId(String id);
+	public boolean idChk(String id);
 }
