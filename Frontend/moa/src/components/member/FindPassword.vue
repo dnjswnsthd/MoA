19435e367ed3f0d06be253e20578ab4bd7e6d0d1
minju75 <68409296+minju75@users.noreply.github.com>
2021-01-27 17:14:27 +0900

    0127 findpassword, login
    findpassword, login 이미지 삽입
    컨테이너 크기 맞추기

--- Frontend/moa/src/components/member/FindPassword.vue
+++ Frontend/moa/src/components/member/FindPassword.vue
@@ -1,20 +1,33 @@
 <template>
 <v-card>
-        <v-card-title class="headline grey lighten-2">
+    <div>
+        <v-spacer></v-spacer>
+        <img src="@/assets/images/main/logo(Bg).png" alt="로고" class="findPasswordImg">
+        <v-spacer></v-spacer>
+        <h2 class="titleText grey lighten-2">비밀번호 찾기</h2>
+    </div>
+    
+        <!-- <v-card-title class="headline grey lighten-2 titleText">
             비밀번호 찾기
-        </v-card-title>
-
+        </v-card-title> -->
+    <div class="findPasswordWidth">
         <v-row>
-        <form class="col-11 formBox">
-               <v-text-field label="ID" type="text" class="col-8"></v-text-field>
+        <form class="col-9 formBox">
+            <v-text-field label="ID" type="text" class="col-8"></v-text-field>
         </form>
         </v-row>
-
-        <div>
+    </div>
+        <div class="findPasswordWidth">
+        <v-row>
+        <v-spacer></v-spacer>
             <v-btn>이메일로 찾기</v-btn>
+        <v-spacer></v-spacer>
             <v-btn>휴대폰번호로 찾기</v-btn>
+        <v-spacer></v-spacer>
+        </v-row>
         </div>
-        <v-card-actions>
+        <br>
+        <v-card-actions class="findPasswordWidth">
         <v-spacer></v-spacer>
             <v-btn
                 color="primary"
@@ -23,6 +36,7 @@
             >
             회원가입
             </v-btn>
+        <v-spacer></v-spacer>
             <v-btn
                 color="primary"
                 text
@@ -30,6 +44,31 @@
             >
             로그인
             </v-btn>
+        <v-spacer></v-spacer>
             </v-card-actions>
         </v-card>
 </template>
+
+<script>
+export default {
+    name: "FindPassword"
+}
+</script>
+
+<style>
+    .titleText {
+        text-align: center;
+    }
+    .findPasswordImg {
+        margin: 0 auto;
+        width: 450px;
+        height: 100x;
+    }
+    form {
+        width: 450px;
+    }
+    .findPasswordWidth {
+        width: 450px;
+        margin: 0 auto;
+    }
+</style>
