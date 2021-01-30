8d3d416c324f654b444165a688f6d6140cc51142
minju75 <68409296+minju75@users.noreply.github.com>
2021-01-30 16:08:57 +0900

    0130 MyPage
    마이페이지 틀 구성, 서브헤더에 마이페이지 카테고리 추가, member.css에 class 추가

--- Frontend/moa/src/components/member/MyPage.vue
+++ Frontend/moa/src/components/member/MyPage.vue
@@ -1,21 +1,64 @@
 <template>
-    <v-card>
+    <div class="width-1000 height-500 mypageMargin">
         <div>
-            
+        <v-row>
         <v-spacer></v-spacer>
-        <img src="@/assets/images/main/logo(Bg).png" alt="로고" class="loginImg">
+        <h1>내 정보</h1>
         <v-spacer></v-spacer>
-
-        <h2 class="titleText grey lighten-2">My Page</h2>
+        <div class="width-500"></div>
+        <h3 class="mypagePoint centerText">포인트: P</h3>
+        <v-spacer></v-spacer>
+        </v-row>
         </div>
 
-        <form class="col-9 formBox">
-            <v-text-field label="ID" type="text" class="col-8"></v-text-field>
-            <v-text-field label="NAME" type="text" class="col-8"></v-text-field>
-            <v-text-field label="AGE" type="text" class="col-8"></v-text-field>
-            <v-text-field label="MAJOR" type="text" class="col-8"></v-text-field>
+        <form class="col-9 formBox width-700">
+            <v-text-field label="ID" type="text" class="col-8 width-700"></v-text-field>
+        <div>
+            <v-row class="width-700" >
+            <v-spacer></v-spacer>
+            <v-text-field label="NAME" type="text" class="col-8 width-300"></v-text-field>
+            <div class="width-100"></div>
+            <v-text-field label="AGE" type="text" class="col-8 width-250"></v-text-field> 
+            <v-spacer></v-spacer>   
+            </v-row>
+        </div>    
+            <v-text-field label="MAJOR" type="text" class="col-8 width-700"></v-text-field>
         </form>
-    </v-card>
+    <div>
+    <v-row>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="primary"
+            text
+        >
+        <router-link to="/">
+        비밀번호 변경
+        </router-link>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-btn
+        color="primary"
+        text
+        >
+        <router-link to="/">
+        내 정보 변경
+        </router-link>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+        <v-btn
+        color="primary"
+        text
+        >
+        <router-link to="/">
+        탈퇴하기
+        </router-link>
+        </v-btn>
+        <v-spacer></v-spacer>
+    </v-row>
+    </div>
+    </div>     
 </template>
 
 
@@ -26,5 +69,8 @@ export default {
 </script>
 
 <style>
-
+    .mypageMargin {
+        margin: 0 auto;
+        padding: 50px;
+    }
 </style>
