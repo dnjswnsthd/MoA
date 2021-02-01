e754ec13e71564ddc938bfee5f627e59080d374b
조석준 <vcxttlvcx@naver.com>
2021-02-01 19:22:03 +0900

    주석 달기
    - 메소드에 대한 설명 주석

--- Backend/src/main/java/com/moa/model/mapper/MemberMapper.java
+++ Backend/src/main/java/com/moa/model/mapper/MemberMapper.java
@@ -13,18 +13,23 @@ public interface MemberMapper {
 	public int idChk(String id) throws SQLException;
 
 	public void join(Map<String, Object> param) throws SQLException;
+
 	public void joinMentor(Map<String, Object> param) throws SQLException;
+
 	public void joinMentee(Map<String, Object> param) throws SQLException;
-	
+
 	public void delete(Map<String, Object> param) throws SQLException;
-	
+
 	public void memberUpdate(MemberDto memberDto) throws SQLException;
+
 	public MemberDto memberSearch(MemberDto memberDto) throws SQLException;
 
 	public void updateTempPassword(Map<String, String> map) throws SQLException;
 
 	public MemberDto memberInfo(String id) throws SQLException;
+
 	public MentorDto mentorInfo(String id) throws SQLException;
+
 	public MenteeDto menteeInfo(String id) throws SQLException;
 
 	public int getStatus(String id) throws SQLException;
