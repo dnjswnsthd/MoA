83ded1fcb532cfc2aafb159e75979d0e0e7c727d
조석준 <vcxttlvcx@naver.com>
2021-02-01 19:22:03 +0900

    주석 달기
    - 메소드에 대한 설명 주석

--- Backend/src/main/java/com/moa/controller/ProjectController.java
+++ Backend/src/main/java/com/moa/controller/ProjectController.java
@@ -27,82 +27,103 @@ import io.swagger.annotations.ApiParam;
 public class ProjectController {
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
-	
+
 	@Autowired
 	private ProjectService projectService;
-	
+
+	/**
+	 * 펀딩 오픈 컨트롤러
+	 * 
+	 * @param project	오픈할 프로젝트의 정보가 담긴 Dto
+	 * @return	오픈 '성공' or '실패' 메시지
+	 */
 	@ApiOperation(value = "펀딩 생성", notes = "펀딩 생성 결과 메시지를 반환한다.", response = Map.class)
 	@PostMapping("/create")
-	//펀딩 오픈 컨트롤러
 	public ResponseEntity<Map<String, Object>> create(
-			@RequestBody @ApiParam(value = "펀딩 생성에 필요한 정보", required = true) ProjectDto project){
+			@RequestBody @ApiParam(value = "펀딩 생성에 필요한 정보", required = true) ProjectDto project) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		HttpStatus status = null;
-		
+
 		try {
 			projectService.create(project);
 			resultMap.put("message", SUCCESS);
 			status = HttpStatus.ACCEPTED;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			resultMap.put("message", FAIL);
 			status = HttpStatus.ACCEPTED;
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	//펀딩 신청 컨트롤러
+
+	/**
+	 * 펀딩 신청 컨트롤러
+	 * 
+	 * @param param	신청한 펀딩의 프로젝트 번호와 신청자의 아이디 정보
+	 * @return	신청 '성공' or '실패' 메시지 전달
+	 */
 	@ApiOperation(value = "펀딩 신청 대기", notes = "펀딩 신청 후 수락까지 대기 중 DB에 상태 저장을 위함.", response = Map.class)
 	@PostMapping("/waiting")
 	public ResponseEntity<Map<String, Object>> waiting(
-			@RequestBody @ApiParam(value = "펀딩 신청  후 대기중인 내역 DB에 추가 ") Map<String, Object> param){
+			@RequestBody @ApiParam(value = "펀딩 신청  후 대기중인 내역 DB에 추가 ") Map<String, Object> param) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		HttpStatus status = null;
 		try {
 			projectService.waiting(param);
 			resultMap.put("messagae", SUCCESS);
 			status = HttpStatus.ACCEPTED;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			resultMap.put("message", FAIL);
 			status = HttpStatus.ACCEPTED;
 		}
-		return new ResponseEntity <Map<String,Object>>(resultMap, status);
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	
-	//신청 허가 컨트롤러(허가 하면 각각 대기에선 삭제 프로젝트 목록에서는 추가 됩니다.)
+
+	/**
+	 * 신청 허가 컨트롤러(허가 하면 각각 대기에선 삭제 프로젝트 목록에서는 추가 됩니다.)
+	 * 
+	 * @param param	 허가할 프로젝트 번호와 신청자의 아이디 정보
+	 * @return	성공 실패 메세지 전달
+	 */
 	@ApiOperation(value = "신청 허가", notes = "신청 허가 후 waiting_project에선 삭제하고  project_member 테이블에 추가", response = Map.class)
 	@PutMapping("/permission")
 	public ResponseEntity<Map<String, Object>> permissiont(
-			@RequestBody @ApiParam(value = "신청 허가 됬을 경우 DB 상태 처리") Map<String, Object> param){
+			@RequestBody @ApiParam(value = "신청 허가 됬을 경우 DB 상태 처리") Map<String, Object> param) {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		HttpStatus status = null;
 		try {
 			projectService.permission(param);
 			resultMap.put("message", SUCCESS);
 			status = HttpStatus.ACCEPTED;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			resultMap.put("message", FAIL);
 			status = HttpStatus.ACCEPTED;
 		}
-		return new ResponseEntity <Map<String,Object>>(resultMap, status);
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	
-	
-	
-	@ApiOperation(value = "진행 중인 펀딩 정보 보기", notes ="마이 페이지에서 유저의 펀딩 정보 보기", response = Map.class)
+	/**
+	 * 자신이 참여하고 있는 펀딩의 정보를 모두 가지고 온다
+	 * 
+	 * @param id	자신의 아이디 정보
+	 * @return	자신이 참여하고 있는 펀딩의 정보와 '성공' 메세지
+	 */
+	@ApiOperation(value = "진행 중인 펀딩 정보 보기", notes = "마이 페이지에서 유저의 펀딩 정보 보기", response = Map.class)
 	@GetMapping("/proceeding/{id}")
-	public ResponseEntity<Map<String, Object>> projectInfo(@PathVariable("id") @ApiParam(value = "펀딩 정보를 불러올 아이디", required = true) String id){
+	public ResponseEntity<Map<String, Object>> projectInfo(
+			@PathVariable("id") @ApiParam(value = "펀딩 정보를 불러올 아이디", required = true) String id) {
 		Map<String, Object> resultMap = new HashMap<>();
 		HttpStatus status = null;
 		try {
+			// DB 에서 ID를 통해 자신이 참여하고 있는 프로젝트의 정보를 가지고 온다
 			ProjectDto projectInfo = projectService.projectInfo(id);
 			resultMap.put("message", SUCCESS);
 			resultMap.put("projectInfo", projectInfo);
 			status = HttpStatus.ACCEPTED;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			resultMap.put("message", e.getMessage());
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		
-		return new ResponseEntity<Map<String,Object>>(resultMap, status);
+
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	
+
 }
--- Backend/src/main/java/com/moa/model/service/MemberService.java
+++ Backend/src/main/java/com/moa/model/service/MemberService.java
@@ -6,10 +6,13 @@ import com.moa.model.MemberDto;
 
 public interface MemberService {
 	public MemberDto login(MemberDto memberDto) throws Exception;
-	
+
 	public boolean idChk(String id) throws Exception;
+
 	public void join(Map<String, Object> param) throws Exception;
-	public void delete(Map<String, Object> param)throws Exception; 
+
+	public void delete(Map<String, Object> param) throws Exception;
+
 	public MemberDto memberUpdate(MemberDto memberDto) throws Exception;
 
 	public void updateTempPassword(String email) throws Exception;
