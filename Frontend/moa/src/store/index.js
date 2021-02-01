318b5da2afc66eb94ad1d03b3ea537892889d323
JuneHyung <cjh951114@naver.com>
2021-02-01 11:14:15 +0900

    jwt추가
    jwt추가 및 로그인위한 파일 추가

--- Frontend/moa/src/store/index.js
+++ Frontend/moa/src/store/index.js
@@ -1,11 +1,52 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import jwt_decode from "jwt-decode";
+import { findById } from "@/api/user.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    isLogin: false, // 로그인 여부
+    memberInfo: null
+  },
+  mutations: {
+    setIsLogined(state, isLogin) {
+      state.isLogin = isLogin;
+    },
+    setMemberInfo(state, memberInfo) {
+      state.isLogin = true;
+      state.memberInfo = memberInfo;
+    },
+    logout(state) {
+      state.isLogin = false;
+      state.memberInfo = null;
+    }
+  },
+  actions: {
+    async GET_MEMBER_INFO({ commit }, token) {
+      let decode = jwt_decode(token);
+      decode.id
+      await findById(
+        decode.id,
+        (response) => {
+          if (response.data.message === "success") {
+            commit("setMemberInfo", response.data.memberInfo);
+            // router.push("/");
+            // router.go(router.currentRoute);
+          } else {
+            console.log("유저 정보 없음!!");
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    LOGOUT({ commit }) {
+      commit("logout");
+      localStorage.removeItem("access-token");
+      // axios.defaults.headers.common["auth-token"] = undefined;
+    }
+  },
   modules: {}
 });
