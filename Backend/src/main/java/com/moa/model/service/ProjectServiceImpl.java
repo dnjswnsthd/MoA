c1c5f2fd44ae46b6adf2bd5f2bb19d93b0d73f76
dnjswnsthd <37327829+dnjswnsthd@naver.com>
2021-02-01 17:35:27 +0900

    ProjectController
    ProjectController 및 구성
    펀딩 오픈까지 구현

---
+++ Backend/src/main/java/com/moa/model/service/ProjectServiceImpl.java
@@ -0,0 +1,21 @@
+package com.moa.model.service;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moa.model.ProjectDto;
+import com.moa.model.mapper.ProjectMapper;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public void create(ProjectDto project) throws Exception{
+		sqlSession.getMapper(ProjectMapper.class).create(project);	
+	}
+
+
+}
