519e0d1b694bd0155f8c90cc39001f4e77412bac
조석준 <vcxttlvcx@naver.com>
2021-02-01 16:26:34 +0900

    로그인 세션 유지
    - Header가 만들어질 때 localStorage를 확인해서 로그인 여부 판단

--- Frontend/moa/src/components/common/SubHeader.vue
+++ Frontend/moa/src/components/common/SubHeader.vue
@@ -1,45 +1,47 @@
 <template>
-    <header >
-        <v-container class="col-8">
-             <v-row v-if="isLogin !== false">
-                <div>
-                    <router-link to="/">
-                    <img src="@/assets/images/main/logo.png" alt="로고" style="width: 150px; height: 60px;">
-                    </router-link>
-                </div>
-                <v-spacer></v-spacer>                
-                    <li class="width-120 centerText height-70">
-                    <router-link to="/FundingOpen">펀딩 오픈</router-link>
-                    </li>
-                    <li class="width-120 centerText height-70">
-                    <router-link to="/MyPage">My Page</router-link>
-                    </li>
-                    <li class="width-120 centerText height-70" @click.prevent="onClickLogout">
-                    LOGOUT
-                    </li>
-            </v-row>
-            <v-row v-else>
-                <div>
-                    <router-link to="/">
-                    <img
-                        src="@/assets/images/main/logo.png"
-                        alt="로고"
-                        style="width: 150px; height: 70px;"
-                    />
-                    </router-link>
-                </div>
-                <v-spacer></v-spacer>
-                <li class="width-120 centerText height-70">
-                    <router-link to="/join">회원가입</router-link>
-                </li> 
-                <li class="width-120 centerText height-70">
-                    <router-link to="/login">로그인</router-link>                         
-                </li>
-            </v-row>               
-        </v-container>
-
-        
-    </header>
+  <header>
+    <v-container class="col-8">
+      <v-row v-if="isLogin !== false">
+        <div>
+          <router-link to="/">
+            <img
+              src="@/assets/images/main/logo.png"
+              alt="로고"
+              style="width: 150px; height: 60px;"
+            />
+          </router-link>
+        </div>
+        <v-spacer></v-spacer>
+        <li class="width-120 centerText height-70">
+          <router-link to="/FundingOpen">펀딩 오픈</router-link>
+        </li>
+        <li class="width-120 centerText height-70">
+          <router-link to="/MyPage">My Page</router-link>
+        </li>
+        <li class="width-120 centerText height-70" @click.prevent="onClickLogout">
+          LOGOUT
+        </li>
+      </v-row>
+      <v-row v-else>
+        <div>
+          <router-link to="/">
+            <img
+              src="@/assets/images/main/logo.png"
+              alt="로고"
+              style="width: 150px; height: 70px;"
+            />
+          </router-link>
+        </div>
+        <v-spacer></v-spacer>
+        <li class="width-120 centerText height-70">
+          <router-link to="/join">회원가입</router-link>
+        </li>
+        <li class="width-120 centerText height-70">
+          <router-link to="/login">로그인</router-link>
+        </li>
+      </v-row>
+    </v-container>
+  </header>
 </template>
 
 <script>
@@ -49,13 +51,16 @@ export default {
   computed: {
     ...mapState(['memberInfo', 'isLogin']),
   },
+  created() {
+    if (localStorage.getItem('access-token') != null) this.$store.commit('setIsLogined', true);
+  },
   methods: {
     onClickLogout() {
       this.$store
         .dispatch('LOGOUT')
         .then(() => {
           // this.$router.push({ name: "" });
-          
+
           if (this.$route.path !== '/') this.$router.replace('/');
         })
         .catch(() => {
@@ -67,13 +72,13 @@ export default {
 </script>
 
 <style>
-header{
-    border-bottom:1px solid black;
+header {
+  border-bottom: 1px solid black;
 }
-    header .container .row li {
-        line-height: 70px;
-    }
-    header .container .row li a {
-        color: black;
-    }
+header .container .row li {
+  line-height: 70px;
+}
+header .container .row li a {
+  color: black;
+}
 </style>
