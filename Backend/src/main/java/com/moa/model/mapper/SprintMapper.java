1b95fbdb73ad6ca8a2afa865abd068be81fbb243
dnjswnsthd <37327829+dnjswnsthd@naver.com>
2021-02-02 14:08:23 +0900

    sprint 상태 변경 구현
    sprint 상태 변경 구현

--- Backend/src/main/java/com/moa/model/mapper/SprintMapper.java
+++ Backend/src/main/java/com/moa/model/mapper/SprintMapper.java
@@ -2,6 +2,10 @@ package com.moa.model.mapper;
 
 import java.util.Map;
 
+import com.moa.model.SprintDto;
+
 public interface SprintMapper {
 	void add(Map<String, Object> param);
+
+	void modify(SprintDto sprintDto);
 }
--- Backend/src/main/java/com/moa/model/service/SprintService.java
+++ Backend/src/main/java/com/moa/model/service/SprintService.java
@@ -2,8 +2,12 @@ package com.moa.model.service;
 
 import java.util.Map;
 
+import com.moa.model.SprintDto;
+
 public interface SprintService {
 
 	public void add(Map<String, Object> param) throws Exception;
+
+	public void modify(SprintDto sprintDto) throws Exception;
 
 }
