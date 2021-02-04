e98e001f9175a666d2cec223477013de4f4de5be
조석준 <vcxttlvcx@naver.com>
2021-02-04 17:21:55 +0900

    코드 정리

--- Backend/src/main/java/com/moa/model/service/MemberServiceImpl.java
+++ Backend/src/main/java/com/moa/model/service/MemberServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.moa.model.MemberDto;
-import com.moa.model.MentorDto;
 import com.moa.model.mapper.MemberMapper;
 
 @Service
@@ -165,18 +164,18 @@ public class MemberServiceImpl implements MemberService {
 		sqlSession.getMapper(MemberMapper.class).delete(param);
 	}
 
-	
 	/**
 	 * @param id, pw 비교할 pw와 정보 변경을 하려는 id
 	 * 
 	 */
 	@Override
-	public boolean pwcheck(String id,String pw) {
-		if(sqlSession.getMapper(MemberMapper.class).pwcheck(id).equals(pw))
+	public boolean pwcheck(String id, String pw) throws Exception {
+		if (sqlSession.getMapper(MemberMapper.class).pwcheck(id).equals(pw))
 			return true;
-		else return false;
+		else
+			return false;
 	}
-		
+
 	@Override
 	public void memberUpdatePoint(MemberDto memberDto) throws SQLException {
 		// 회원정보 수정은 이미 로그인이 된 상태로 진행 가능
