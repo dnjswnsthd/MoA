6d9321876ec5758697795c34db1e5191ef3469de
조석준 <vcxttlvcx@naver.com>
2021-02-04 16:46:39 +0900

    DB 선택문 추가

--- Backend/src/main/resources/static/MoA_db.sql
+++ Backend/src/main/resources/static/MoA_db.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `complete_project`
 --
 
+USE moa;
+
 DROP TABLE IF EXISTS `complete_project`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
