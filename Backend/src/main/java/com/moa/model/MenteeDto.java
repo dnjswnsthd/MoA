ede36970e87e990089b95e94c707b37dd43c2860
dnjswnsthd <dnjswnsthd@naver.com>
2021-02-05 14:22:18 +0900

    Swagger ApiModel 제거

--- Backend/src/main/java/com/moa/model/MenteeDto.java
+++ Backend/src/main/java/com/moa/model/MenteeDto.java
@@ -1,8 +1,5 @@
 package com.moa.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 /**
  * 멘티 Dto 구현
  * MemberDto 상속 받아 구현
@@ -10,22 +7,16 @@ import io.swagger.annotations.ApiModelProperty;
  * @author 조석준
  * 작성일 : 2021-01-26
  */
-@ApiModel(value = "멘티 정보", description = "멘티의 보유 기술, 능력치 정보")
 public class MenteeDto extends MemberDto {
 	// 보유 기술
-	@ApiModelProperty(value = "보유 기술")
 	private String skill;
 	// 능력치
-	@ApiModelProperty(value = "소통력")
 	private int communication;	// 소통력
-	@ApiModelProperty(value = "책임감")
 	private int responsibility;	// 책임감
-	@ApiModelProperty(value = "구현 능력")
 	private int performance;	// 구현 능력
-	@ApiModelProperty(value = "긍정")
-	private int positive;		//
-	@ApiModelProperty(value = "리더십")
+	private int positive;		//적극성
 	private int leadership;		// 리더쉽
+	
 	// getter, setter
 	public String getSkill() {
 		return skill;
