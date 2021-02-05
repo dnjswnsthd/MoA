ae10e85859de2c1e66b085eaa0b6d2de6145799b
JuneHyung <cjh951114@naver.com>
2021-02-05 16:04:25 +0900

    펀딩오픈 - 프로젝트 만들기
    펀딩오픈 - 프로젝트 만들기

--- Frontend/src/components/funding/Schedule.vue
+++ Frontend/src/components/funding/Schedule.vue
@@ -1,112 +1,97 @@
 <template>
-    <div class="col-8 centerContent">
-        <div>
-            <h1 class="height-50">name</h1>
-            <h4 class="height-50">주제:</h4>
-            <h4 class="height-50">일정:</h4>
-        </div>
-        <div v-for="item in items" :key="item">
-            <div class="sceduleBorder"></div>
-            <div class="pt-3">
-                <div class="col-9">
-                    <v-row>
-                        <h2>{{ item }}</h2>
-                        <v-dialog v-model="dialog" persistent max-width="290">
-                            <template v-slot:activator="{ on, attrs }">
-                                <v-btn
-                                    class="mx-2"
-                                    dark
-                                    small
-                                    color="#AB47BC"
-                                    v-bind="attrs"
-                                    v-on="on"
-                                >
-                                    <v-icon dark>
-                                        mdi-plus
-                                    </v-icon>
-                                </v-btn>
-                            </template>
-                            <v-card>
-                                <v-card-title class="headline">
-                                    어떤 일정을 추가하시겠어요?
-                                </v-card-title>
-                                <v-text-field
-                                    id="제목"
-                                    type="text"
-                                    label="제목"
-                                    class="width-250 fundingMargin"
-                                ></v-text-field>
-                                <v-text-field
-                                    id="내용"
-                                    type="text"
-                                    label="내용"
-                                    class="width-250 fundingMargin"
-                                ></v-text-field>
-                                <v-text-field
-                                    id="담당자"
-                                    type="text"
-                                    label="담당자"
-                                    class="width-150 ml-6"
-                                ></v-text-field>
-                                <!-- <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text> -->
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn
-                                        color="#845ec2"
-                                        text
-                                        @click="dialog = false"
-                                    >
-                                        취소
-                                    </v-btn>
-                                    <v-btn
-                                        color="#845ec2"
-                                        text
-                                        @click="dialog = True"
-                                    >
-                                        추가
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                    </v-row>
-                </div>
-                <v-sheet class="mx-auto" max-width="1250">
-                    <v-slide-group
-                        v-model="model"
-                        class="pa-4"
-                        active-class="success"
-                        show-arrows
-                    >
-                        <v-slide-item
-                            class="mr-5"
-                            v-for="index in 5"
-                            :key="index"
-                        >
-                            <schedule-card
-                                :index="index"
-                                :item="item"
-                            ></schedule-card>
-                        </v-slide-item>
-                    </v-slide-group>
-                </v-sheet>
-            </div>
-        </div>
+  <div class="col-8 centerContent">
+    <div>
+      <h1 class="height-50">name</h1>
+      <h4 class="height-50">주제:</h4>
+      <h4 class="height-50">일정:</h4>
     </div>
+    <div v-for="item in items" :key="item">
+      <div class="sceduleBorder"></div>
+      <div class="pt-3">
+        <div class="col-9">
+          <v-row>
+            <h2>{{ item }}</h2>
+            <v-dialog v-model="dialog" persistent max-width="290">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  class="mx-2"
+                  dark
+                  small
+                  color="#AB47BC"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon dark>
+                    mdi-plus
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline">
+                  어떤 일정을 추가하시겠어요?
+                </v-card-title>
+                <v-text-field
+                  id="제목"
+                  type="text"
+                  label="제목"
+                  class="width-250 fundingMargin"
+                ></v-text-field>
+                <v-text-field
+                  id="내용"
+                  type="text"
+                  label="내용"
+                  class="width-250 fundingMargin"
+                ></v-text-field>
+                <v-text-field
+                  id="담당자"
+                  type="text"
+                  label="담당자"
+                  class="width-150 ml-6"
+                ></v-text-field>
+                <!-- <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text> -->
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="#845ec2" text @click="dialog = false">
+                    취소
+                  </v-btn>
+                  <v-btn color="#845ec2" text @click="dialog = True">
+                    추가
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+        </div>
+        <v-sheet class="mx-auto" max-width="1250">
+          <v-slide-group
+            v-model="model"
+            class="pa-4"
+            active-class="success"
+            show-arrows
+          >
+            <v-slide-item class="mr-5" v-for="index in 5" :key="index">
+              <schedule-card :index="index" :item="item"></schedule-card>
+            </v-slide-item>
+          </v-slide-group>
+        </v-sheet>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import ScheduleCard from "./ScheduleCard.vue";
 
 export default {
-    components: {
-        ScheduleCard,
-    },
-    data() {
-        return {
-            dialog: false,
-            items: ["할 일", "진행 중", "완료"],
-        };
-    },
+  components: {
+    ScheduleCard
+  },
+  data() {
+    return {
+      dialog: false,
+      items: ["할 일", "진행 중", "완료"]
+    };
+  }
 };
 </script>
 
--- Frontend/src/util/http-common.js
+++ Frontend/src/util/http-common.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 // axios 객체 생성
 export default axios.create({
-    // baseURL: 'http://localhost:8000/moa/',
-    baseURL: 'https://i4d111.p.ssafy.io/moa/',
-    headers: {
-        'Content-type': 'application/json',
-    },
+  // baseURL: 'http://localhost:8000/moa/',
+  baseURL: "https://i4d111.p.ssafy.io/moa/",
+  headers: {
+    "Content-type": "application/json"
+  }
 });
