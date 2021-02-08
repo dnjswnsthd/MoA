fe00f18b93f7908b70201782042939a32492e98e
조석준 <vcxttlvcx@naver.com>
2021-02-08 23:45:14 +0900

    평가 항목 등록 Back 수정2
    - Dto 수정

--- Backend/src/main/java/com/moa/model/EvaluateDto.java
+++ Backend/src/main/java/com/moa/model/EvaluateDto.java
@@ -7,11 +7,19 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "평가 등록을 위한 Dto", description = "멘토에 대한 평가와 멘티들에 대한 평가가 담겨 온다")
 public class EvaluateDto {
+	@ApiModelProperty(value = "프로젝트 번호")
+	private int project_num;
 	@ApiModelProperty(value = "평가를 위한 멘토의 정보")
 	private MentorDto mentor;
 	@ApiModelProperty(value = "평가를 위한 멘티들의 정보")
 	private List<MenteeDto> mentees;
 	
+	public int getProject_num() {
+		return project_num;
+	}
+	public void setProject_num(int project_num) {
+		this.project_num = project_num;
+	}
 	public MentorDto getMentor() {
 		return mentor;
 	}
@@ -24,6 +32,4 @@ public class EvaluateDto {
 	public void setMentees(List<MenteeDto> mentees) {
 		this.mentees = mentees;
 	}
-	
-	
 }
